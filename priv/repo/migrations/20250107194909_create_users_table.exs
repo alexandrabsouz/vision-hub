b7446687ac0626e715c1fe05550ef20554bc6709
Alexandra Souza <alexandrabsouz@gmail.com>
2025-01-07 16:50:30 -0400

    feat: create users table migration

---
+++ priv/repo/migrations/20250107194909_create_users_table.exs
@@ -0,0 +1,19 @@
+defmodule VisionHub.Repo.Migrations.CreateUsersTable do
+  @moduledoc """
+  This migration creates the users table with email, name, and is_active fields.
+  """
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :email, :string, null: false, size: 50
+      add :name, :string, null: false, size: 50
+      add :is_active, :boolean, default: true, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
