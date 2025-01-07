5f4212c238adba71e3408f6e653d02429d2faf70
Alexandra Souza <alexandrabsouz@gmail.com>
2025-01-07 16:48:02 -0400

    feat: create devices table

---
+++ lib/vision_hub/account/device.ex
@@ -0,0 +1,44 @@
+defmodule VisionHub.Device do
+  @moduledoc """
+  The `VisionHub.Device` module defines the schema and changesets for the Device entity in the VisionHub application.
+
+  This module uses Ecto to interact with the database and provides functions to validate and manipulate device data.
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias VisionHub.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @required_params [:brand, :user_id]
+  @derive {Jason.Encoder, only: [:id, :brand, :is_active, :user_id]}
+
+  schema "devices" do
+    field :brand, :string
+    field :is_active, :boolean
+
+    belongs_to :user, User
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset for creating a device.
+  """
+  def build(changeset), do: apply_action(changeset, :create)
+
+  @doc """
+  Creates a changeset based on the `struct` and `params`.
+
+  ## Examples
+
+      iex> changeset(%VisionHub.Device{}, %{brand: "BrandName", is_active: true, user_id: "some-user-id"})
+      %Ecto.Changeset{...}
+
+  """
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params ++ [:is_active])
+    |> validate_required(@required_params)
+  end
+end
