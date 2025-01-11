0a2ebee84f989c3756d32c04dee8e60affecaca3
Alexandra Souza <alexandrabsouz@gmail.com>
2025-01-11 15:51:21 -0400

    feat: add name in devices

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -18,32 +18,36 @@ alias VisionHub.Accounts.Device
 current_time = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
 brands = ["Intelbras", "Hikvision", "Giga", "Vivotek"]
 
-# Gerar usuários
-user_data = Enum.map(1..1000, fn _ ->
-  %{
-    email: Faker.Internet.email(),
-    name: Faker.Person.name(),
-    inserted_at: current_time,
-    updated_at: current_time
-  }
-end)
+user_data =
+  Enum.map(1..1000, fn _ ->
+    %{
+      email: Faker.Internet.email(),
+      name: Faker.Person.name()
+    }
+  end)
 
-# Executando inserções sequenciais dentro de transações
 Enum.each(user_data, fn user_data ->
   Repo.transaction(fn ->
-    # Inserir usuário
     user = Repo.insert!(User.changeset(%User{}, user_data))
 
-    # Gerar e inserir dispositivos
-    devices_data = Enum.map(1..50, fn _ ->
-      %{
-        brand: Enum.random(brands),
-        is_active: Enum.random([true, false]),
-        user_id: user.id,
-        inserted_at: current_time,
-        updated_at: current_time
-      }
-    end)
+    devices_data =
+      Enum.map(1..50, fn _ ->
+        %{
+          brand: Enum.random(brands),
+          is_active: Enum.random([true, false]),
+          name:
+            Enum.random([
+              "Device Home",
+              "Device Work",
+              "Device Office",
+              "Device Warehouse",
+              "Device Store"
+            ]),
+          user_id: user.id,
+          inserted_at: current_time,
+          updated_at: current_time
+        }
+      end)
 
     Repo.insert_all(Device, devices_data)
   end)
